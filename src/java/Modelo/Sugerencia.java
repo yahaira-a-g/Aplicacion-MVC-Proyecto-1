14478e4c669c6bcd1ea32e9cad1bb84e5ff2cc31
Yahaira Adama <yahairaadamagalarza@gmail.com>
2020-08-03 09:46:58 -0500

    Arquitectura MVC - Con Archivos Listossss

--- src/java/Modelo/Sugerencia.java
+++ src/java/Modelo/Sugerencia.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ModeloDAO;
+package Modelo;
 
 /**
  *
  * @author Usuario
  */
-public class EstudianteDAO {
+public class Sugerencia {
     
 }
--- src/java/ModeloDAO/SugerenciaDAO.java
+++ src/java/ModeloDAO/SugerenciaDAO.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ModeloDAO;
 
 /**
  *
  * @author Usuario
  */
-public class EstudianteDAO {
+public class SugerenciaDAO {
     
 }
